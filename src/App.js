d58dc344321234c98795db0a118aeb99f248bba6
Daniel Quinn <dandidit.quinn@gmail.com>
2023-05-15 17:05:38 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  // handleCitySubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     //CHANGE THIS
+  //     let city = await axios.get(url);
+  //     this.setState({
+  //       error: false
+  //     });
+  //   }
+  //   catch (error) {
+  //     this.setState({
+  //       error: true,
+  //       errorMessage: `An error Occured: ${error.response.status}`
+  //     });
+  //   }
+  // }
+
+  handleLocationSubmit = async (e) => {
+    e.preventDefault();
+
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`;
+
+    let cityData = await axios.get(url);
+
+    // proof of life:
+    console.log(cityData.data);
+
+    this.setState({
+      cityData: cityData.data[0]
+    });
+    //  console.log(this.state.cityName);
+  }
+  changeCityInput = (e) => {
+    this.setState({
+      cityName: e.target.value
+    });
+  }
+
+  render() {
+    let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=12`
+
+    return (
+      <>
+        <h1>Data from an API</h1>
+        <form onSubmit={this.handleLocationSubmit}>
+          <label>Search for a City!
+            <input name="city" onChange={this.changeCityInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+

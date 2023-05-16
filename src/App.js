c0b85d10fb9379f20012c801fea773c9f5fd8219
Daniel Quinn <dandidit.quinn@gmail.com>
2023-05-15 19:33:01 -0700

    rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import Card from 'react-bootstrap/Card';
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import { Card } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
   constructor(props) {
@@ -11,46 +10,39 @@ class App extends React.Component {
     this.state = {
       cityName: '',
       cityData: {},
+      haveCityData: false,
       error: false,
       errorMessage: ''
     }
   }
 
-  // handleCitySubmit = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     //CHANGE THIS
-  //     let cityData = await axios.get(url);
-
-  //     // proof of life:
-  //     console.log(cityData.data);
-
-  //     this.setState({
-  //       cityData: cityData.data[0]
-  //     });
-  //     //  console.log(this.state.cityName);
-  //   }
-
-  // changeCityInput = (e) => {
-  //     this.setState({
-  //       cityName: e.target.value
-  //     });
-  //   }
-
-  handleLocationSubmit = async (e) => {
-    e.preventDefault();
-
-    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`;
-
-    let cityData = await axios.get(url);
-
-    // proof of life:
-    console.log(cityData.data);
-
-    this.setState({
-      cityData: cityData.data[0]
-    });
+  // WHEN DEALING WITH AXIOS YOU NEED 3 things:
+  // - 1) async
+  // - 2) await
+  // - 3) .data
+  handleCitySubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let cityUrl = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`;
+      let city = await axios.get(cityUrl);
+      console.log(city);
+      this.setState({
+        Data1: city.data[0],
+        error: false,
+        haveCityData: true
+      });
+    }
+    catch (error) {
+      console.log('error: ', error);
+      console.log('error.message: ', error.message);
+      this.setState({
+        error: true,
+        errorMessage: `An error Occured: ${error.response.status}`
+      });
+    }
   }
+
+
   changeCityInput = (e) => {
     this.setState({
       cityName: e.target.value
@@ -58,28 +50,27 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.cityData.lat);
+    console.log(this.state)
     return (
       <>
-        <header>
-          <h1>CITY EXPLORER</h1>
-          <form onSubmit={this.handleLocationSubmit}>
-            <label>Search for a City!
-              <input name="city" style={{marginLeft:'8px'}}onChange={this.changeCityInput} />
-              <Button  type="submit">Explore!</Button>
-            </label>
-          </form>
-        </header>
-        <main>
+        <h1>Data from an API</h1>
+        <form onSubmit={this.handleCitySubmit}>
+          <label>
+            <input name="city" onChange={this.changeCityInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
+        {this.state.error ? <p>{this.state.errorMessage}</p> :
+          this.state.haveCityData &&
           <Card className='City p-2 h-100%' style={{ width: '75%' }}>
             <Card.Body>
-              <Card.Title className="title">{this.state.cityName}</Card.Title>
-              <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=12`} alt={`Map of ${this.state.cityName}`} />
-              <Card.Text>Latitude: {this.state.cityData.lon}</Card.Text>
-              <Card.Text>Longitude: {this.state.cityData.lat}</Card.Text>
+              <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.Data1.lat},${this.state.Data1.lon}&zoom=12`} alt="" />
+              <Card.Title>{this.state.cityName}</Card.Title>
+              <Card.Text>Lat: {this.state.Data1.lat}</Card.Text>
+              <Card.Text>Lon: {this.state.Data1.lon}</Card.Text>
             </Card.Body>
           </Card>
-        </main>
+        }
       </>
     );
   }
